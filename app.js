8d4af07400feaae14c88247c1b3f6ef70d55689e
Denis M1rxcle <noonebesideu@gmail.com>
2025-01-16 21:52:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const header = document.querySelector("h1 span")
+	header.style.opacity = 1
+	header.style.transform = "translateY(10%)"
+})
